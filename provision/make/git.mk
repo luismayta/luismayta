a812d06d741b7c572c4fe514913e16bcfdf13e69
@slovacus <slovacus@gmail.com>
2021-04-10 00:06:05 -0500

    refactor: actions make (#27)

--- provision/make/git.mk
+++ provision/make/git.mk
@@ -1,30 +1,42 @@
-# git
+## Show commands help git
 .PHONY: git.help
-
-
 git.help:
 	@echo '    git:'
 	@echo ''
 	@echo '        git                 show help'
 	@echo '        git.setup           install dependences to application'
-	@echo '        git.ignore           install dependences to application'
+	@echo '        git.ignore          ignore dependences to application'
+	@echo '        git.reviews         add revieweers to git config'
 	@echo ''
 
+## Show commands git
+.PHONY: git
 git:
 	@if [ -z "${command}" ]; then \
 		make git.help;\
 	fi
 
 
-# setup all actions git ignore.
+## Setup all actions git ignore.
 .PHONY: git.setup
 git.setup:
 	@echo "==> setup git..."
 	make git.ignore
+	make git.reviews
 
-# setup all actions git ignore.
+## Generate git ignore of files.
 .PHONY: git.ignore
 git.ignore:
 	@echo "==> git ignore generated..."
 	@$(GI) ${GIT_IGNORES} > .gitignore
+	@for ignore in ${GIT_IGNORES_CUSTOM} ; do \
+		echo "$${ignore}" >> .gitignore ; \
+    done
+	@echo ${MESSAGE_HAPPY}
+
+## Add reviewers to git config.
+.PHONY: git.reviews
+git.reviews:
+	@echo "==> add issues revievers..."
+	@git config github.reviews "${REVIEWERS}"
 	@echo ${MESSAGE_HAPPY}
